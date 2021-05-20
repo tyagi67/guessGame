4ae26adc32b04de498ece0aa9d44933f5815738d
Anant Tyagi <ananttyagi099@gmail.com>
2021-05-20 13:32:33 +0530

    Initial commit

---
+++ guessNumber.py
@@ -0,0 +1,16 @@
+import random
+
+randNumber = random.randint(1,100)
+userGuess = -1
+guesses = 0
+
+while randNumber != userGuess:
+    userGuess = int(input("Enter your guess[1,100] : "))
+    guesses +=1
+    if userGuess > randNumber:
+        print("You guessed it wrong! Enter a smaller number")
+    elif userGuess < randNumber:
+         print("You guessed it wrong! Enter a larger number")
+    else:
+        print("You guessed it right!")
+print(f"You take "+str(guesses)+" guesses")
